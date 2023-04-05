7d602ca7d2c3be793fe5fbe081cc0e4c1bbfac12
Youngblood, Cameron <cameron.youngblood@gmail.com>
2023-04-05 12:35:40 -0600

    add logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :sessions do
+    collection do
+      delete :destroy, to: "sessions#destroy"
+    end
+  end
+  root "users#new"
 end
