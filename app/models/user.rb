8c9a4c8d351b6becf929991d89960512fe669b12
Youngblood, Cameron <cameron.youngblood@gmail.com>
2023-04-11 14:55:28 -0600

    update styles on devise views and update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-    validates :name, presence: true
     validates :email, presence: true
 
     has_many :user_votes
--- db/schema.rb
+++ db/schema.rb
@@ -35,7 +35,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_11_155821) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
