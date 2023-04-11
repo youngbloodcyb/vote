8b62ee9806e45d2c12a60373d901c7f236868a21
Youngblood, Cameron <cameron.youngblood@gmail.com>
2023-04-11 11:01:38 -0600

    Add devise gem for user auth

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -1,4 +1,5 @@
 class PollsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_poll, only: %i[ show edit update destroy ]
 
   # GET /polls or /polls.json
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :user_votes
   resources :options
   resources :polls
   resources :users
-  Rails.application.routes.draw do
-  resources :user_votes
-  resources :options
-  resources :polls
-    get '/login', to: 'sessions#new'
-    post '/login', to: 'sessions#create'
-    delete '/logout', to: 'sessions#destroy'
-  end
-  root "users#new"
+  root "polls#new"
 end
